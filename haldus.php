1c16112e11a6f589ffa6e30a2b1dac1c8522da0d
Mroosi <madisroosi@hotmail.com>
2016-04-10 22:49:17 +0300

    kodune

---
+++ haldus.php
@@ -0,0 +1,59 @@
+<?php
+header('Content-Type: aplication/json; charset=utf-8');
+// Fail peab olema veebiserveri kasutaja jaoks kirjutatav
+$baas = 'andmebaas.txt';
+
+if ( file_exists($baas)){
+// loeme faili sisu tekstina muutujasse
+$andmebaas = file_get_contents($baas);
+// deserialiseerime teksti JSON formaadist masiiviks
+$andmebaas = json_decode($andmebaas, true);
+} else{
+    $andmebaas = array();
+}
+// järgneva käivitame ainult POST päringu korral
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (isset($_POST['kustuta'])) { // kontrollime kas vormis oli väli nimega "kustuta"
+
+        // eemaldame valitud indeksiga elemendi massiivist
+        $kustuta = intval($_POST['kustuta']);
+      
+    } else {
+            
+        if( !empty($_POST['nimetus'])){
+             $nimetus = $_POST['nimetus'];
+        }else{
+             $nimetus = '';
+        }
+       if( !empty($_POST['kogus'])){
+        $kogus = intval($_POST['kogus']);
+       }else{
+           $kogus=0;
+       }
+        // kontrollime kas sisendväärtused on oodatud kujul või mitte
+        if ($nimetus == '' || $kogus <= 0) {
+            header('HTTP/1.1 400 Bad Request');
+            echo json_encode(array(
+                'error' =>'Vigased väärtused!'
+                ));
+            exit;
+        }
+
+        // lisame kirje "andmebaasi", st. et lisame massiivi uue indeksi koos vormi andmetega
+        $andmebaas[] = array(
+            'nimetus' => $nimetus,
+            'kogus' => $kogus,
+        );
+    }
+
+    // salvestame muudetud massiivi
+    // kõigepealt serialiseerime massiivi JSON stringiks
+    $andmebaas = json_encode($andmebaas);
+    // salvestame serialiseeritud stringi andmebaasifaili
+    file_put_contents($baas, $andmebaas);
+echo $andmebaas;
+   
+}else{
+    echo json_encode($andmebaas);
+}
